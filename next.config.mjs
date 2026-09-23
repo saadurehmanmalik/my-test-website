a57e8d7e5e25e01457eca4e2af941843acf8bcf4
Saad <saadurehmanmalik@hotmail.com>
2026-09-24 03:28:15 +0500

    Fix next config for static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
-/** @type {import("next").NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
 export default nextConfig;
